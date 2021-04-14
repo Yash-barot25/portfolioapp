42496d665f75bc5be99eb8049fe10e7537d559dc
Tristen <janzentristen@yahoo.com>
2021-04-14 13:17:45 -0500

    A simple start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import React, {Component} from 'react'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Print Export Word
       </header>
+      <div className="imageContainer">
+      <img src="https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg" alt="alt" className="profilepic"/>
+      Tristen Janzen<br/>
+      Fullstack Java Developer
+      </div>
+    </div>
     </div>
   );
 }
